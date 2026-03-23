e0744ffb147c70df8e3b41402f99cd71a0f4c94a
AndreyZabavin <106765175+AndreyZabavin@users.noreply.github.com>
2026-03-23 20:31:18 +0300

    Add files via upload

---
+++ day6/ex00/team00_ex00.sql
@@ -0,0 +1,36 @@
+WITH RECURSIVE paths AS (
+    SELECT point1 AS start,
+        point2 AS finish,
+        cost,
+        ARRAY [point1, point2]::text [] AS path
+    FROM nodes
+    WHERE point1 = 'a'
+    UNION
+    SELECT paths.start,
+        nodes.point2,
+        paths.cost + nodes.cost,
+        paths.path || nodes.point2::text
+    FROM paths
+        JOIN nodes ON paths.finish = nodes.point1
+    WHERE NOT nodes.point2 = ANY(paths.path)
+),
+complete_tours AS (
+    SELECT cost + (
+            SELECT cost
+            FROM nodes
+            WHERE point1 = finish
+                AND point2 = start
+        ) AS total_cost,
+        path || start AS tour
+    FROM paths
+    WHERE cardinality(path) = 4
+)
+SELECT total_cost,
+    tour
+FROM complete_tours
+WHERE total_cost = (
+        SELECT MIN(total_cost)
+        FROM complete_tours
+    )
+ORDER BY total_cost,
+    tour;
