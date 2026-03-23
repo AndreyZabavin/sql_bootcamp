7f3b754f003acc1d42beca79666a0c72b83c028b
AndreyZabavin <106765175+AndreyZabavin@users.noreply.github.com>
2026-03-23 20:27:34 +0300

    Add files via upload

---
+++ day2/ex01/day01_ex01.sql
@@ -0,0 +1,8 @@
+SELECT object_name
+FROM (
+	SELECT name AS object_name
+	FROM person
+	UNION ALL
+	SELECT pizza_name AS object_name
+	FROM menu 
+) AS subquery;
---
+++ day2/ex10/day01_ex10.sql
@@ -0,0 +1,9 @@
+SELECT 
+	p.name AS person_name,
+	m.pizza_name,
+	pi.name AS pizzeria_name
+FROM person_order AS po
+JOIN person AS p ON po.person_id = p.id
+JOIN menu AS m ON po.menu_id = m.id
+JOIN pizzeria AS pi ON m.pizzeria_id = pi.id
+ORDER BY person_name, pizza_name, pizzeria_name;
